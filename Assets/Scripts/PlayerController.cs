b49880fcac5783b6f0f7502cc0939637702df5e9
ZAKosma <zakosma@gmail.com>
2022-10-10 14:39:55 -0500

    lets go

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,38 +5,41 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
 
-    public float speed; 
+    public float speed;
+
+    public Rigidbody rb;
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = gameObject.GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        Vector3 playerMovement = transform.position;
+        Vector3 playerMovement = Vector3.zero;
         if (Input.GetKey(KeyCode.W))
         {
             //Move up negative in x
-            playerMovement.x += speed;
+            playerMovement.z += speed;
         }
 
         if (Input.GetKey(KeyCode.S))
         {
-            playerMovement.x -= speed;
+            playerMovement.z -= speed;
         }
 
         if (Input.GetKey(KeyCode.A))
         {
-            playerMovement.z -= speed;
+            playerMovement.x -= speed;
         }
 
         if (Input.GetKey(KeyCode.D))
         {
-            playerMovement.z += speed;
+            playerMovement.x += speed;
         }
 
-        transform.position = playerMovement;
+        rb.AddForce(playerMovement);
     }
 }
